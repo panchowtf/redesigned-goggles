86d574f509d47b80b16e8855b8a0654835a9226e
Francisco Ramírez <fmiguel_rr@hotmail.com>
2017-08-03 09:40:15 -0500

    New branch

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Spring Air México</title>
+    <link rel="icon" type="image/png" href="images/favicon.png">
+
+    <!-- Bootstrap -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body data-0="padding-top:50px;" data-500="padding-top:99px;">
+<nav class="navbar navbar-default navbar-fixed-top"data-0="height:50px;" data-500="height: 100px;">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="#"><img class="brand-logo" src="images/Logo.png" alt="Spring Air México" data-0="position:absolute; top:-120px;" data-500="position:absolute; top:1px;"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+      <ul class="nav navbar-nav navbar-right" data-0="padding-top:0px;font-size:.9em;" data-500="padding-top:20px;font-size:1.4em;">
+        <li><a href="#">Sistemas de descanso</a></li>
+        <li><a href="#">Blog del oso</a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="jumbotron">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6"><h1><img src="images/Logo.png" alt="Spring Air méxico" height="400 px"></h1></div>
+      <div class="col-md-6"><h1 style="color:white;text-shadow: 0px 0px 20px #555;">Tenemos el descanso simplemente perfecto para ti.</h1></div>
+
+   
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6"><img src="images/sleep.png" alt="placeholder" class="img-responsive"></div>
+    <div class="col-md-6"><img src="images/sleep.png" alt="placeholder" class="img-responsive"></div>
+  </div>
+</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+   <script type="text/javascript" src="js/skrollr.min.js"></script>
+	<script type="text/javascript">
+	var s = skrollr.init();
+	</script>
+  </body>
+</html>
